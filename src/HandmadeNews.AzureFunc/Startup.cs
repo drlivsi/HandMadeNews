3f36af7b49c7e662c0439cf95a044eea53dcb332
Viktor Levytskyi <drlivsi2@gmail.com>
2023-08-07 13:14:25 +0300

    minor fixes

--- src/HandmadeNews.AzureFunc/Startup.cs
+++ src/HandmadeNews.AzureFunc/Startup.cs
@@ -70,19 +70,5 @@ namespace HandmadeNews.AzureFunc
                 .AddEnvironmentVariables()
                 .Build();
         }
-
-        //private IConfiguration BuildConfiguration(string applicationRootPath)
-        //{
-        //    var config =
-        //        new ConfigurationBuilder()
-        //            .SetBasePath(applicationRootPath)
-        //            .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
-        //            .AddJsonFile("settings.json", optional: true, reloadOnChange: true)
-        //            .AddEnvironmentVariables()
-        //            .Build();
-
-
-        //    return config;
-        //}
     }
 }
